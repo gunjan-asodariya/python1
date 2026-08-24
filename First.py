ca9e6d4cb5ad24beadf84458ac297c83d7bbf843
gunjan-asodariya <gunjan.asodariya.cg@gmail.com>
2026-08-24 09:59:46 +0530

    changes in asspython file

--- First.py
+++ First.py
@@ -1,3 +1,7 @@
 print("hello world")
 print("that's my first python code")
 print("my name is gunjan asodariya")
+name="gunjan"
+age="18"
+city="gujrat"
+print(name, age, city)
---
+++ asspython.py
@@ -0,0 +1,36 @@
+#que.1
+name="gunjan"
+print(name)
+age=18
+print(age)
+city="surat"
+print(city)
+
+#que.2
+studentname="gunjan"
+studentrollnumber=10
+studentbranch="AI"
+print(studentbranch, studentrollnumber, studentname)
+
+#que.3
+marks=24
+marks=25
+print(marks)
+
+#que4
+name="rahul"
+age=18
+city="patna"
+print(name, age, city)
+
+#que.5
+x=y=z=0
+print(x)
+print(y)
+print(z)
+
+#que.6
+student_name="gunjan"
+student_age=18
+student_age=20
+
