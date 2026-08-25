56f2ee0e438c0b3a0b434a2c353befd60265c10c
gunjan-asodariya <gunjan.asodariya.cg@gmail.com>
2026-08-25 10:04:28 +0530

    added python file

---
+++ ass2python.py
@@ -0,0 +1,44 @@
+student_name="gunjan"
+student_age=18
+student_height="190cm"
+wheter_person_is_a_student= True
+result = None
+print(type(student_name)) 
+print(type(student_age)) 
+print(type(student_height)) 
+print(type(wheter_person_is_a_student)) 
+
+#25
+Marks=50
+marks=50.0
+markS="50"
+print(type(Marks))
+print(type(marks))
+print(type(markS))
+
+#26
+you_are_person=True
+gunjan="True"
+print(type(you_are_person))
+print(type(gunjan))
+
+#27
+result=None
+Result="none"
+print(type(result))
+print(type(Result))
+
+#28
+name="gunjan"
+student_age=18
+age=18.0
+whether_the_product= True
+result= None
+print(type(name))
+print(type(student_age))
+print(type(age))
+print(type(whether_the_product))
+print(type(result))
+
+
+
