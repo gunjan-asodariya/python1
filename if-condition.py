18debf128b72dd410259ef4c075e5a08cadc7729
gunjan-asodariya <gunjan.asodariya.cg@gmail.com>
2026-09-07 23:23:22 +0530

    Add files via upload

---
+++ if-condition.py
@@ -0,0 +1,281 @@
+#1
+
+a=10
+if a>10:
+    print("a is greater than 10")
+
+#2
+age=20
+if age>=18:
+    print("You are eligible to vote")
+
+#3
+b=int(input("Enter your number:"))
+if b > 0:
+    print("You have entered a positive number")
+
+#4
+c=int(input("Enter your marks:"))
+if c >= 40:
+    print("You have passed the exam")
+else:
+    print("You have failed the exam")
+
+#5
+d=int(input("Enter your number:"))
+if d == 0:
+    print("You have entered zero")
+
+#6
+e=int(input("Enter your number:"))
+if e > 0:
+    print("You have entered a positive number")
+else:
+    print("You have entered a negative number")
+
+#7
+age1=int(input("Enter your age:"))
+if age1 >= 18:
+    print("adult")
+else:
+    print("minor")
+
+#8
+a1=int(input("Enter a number: "))
+if a1 % 2 == 0:
+    print("your number is Even")
+else:
+    print("your number is Odd")
+
+#9
+marks1=int(input("Enter your marks: "))
+if marks1 >= 40:
+    print("You have passed the exam")
+else:
+    print("You have failed the exam")
+
+#11
+marks2=int(input("Enter your marks: "))
+if marks2 >= 90:
+    print("You have got A grade")   
+elif marks2 >=75:
+        print("You have got b grade")  
+elif marks2 >=60:
+        print("You have got c grade") 
+elif marks2 >=40:
+        print("You have got d grade")
+else:
+    print("you are fail in exam")  
+
+#12
+
+num=input("input your number")
+if num > 0:
+    print("your number is positive")
+elif num == 0:
+    print("your number is zero")
+else:
+    print("your number is negetive")
+
+#13
+
+num1=input("choose your number(1,2,3,4,5)")
+if num1 == 1:
+    print("you choose monday")
+elif num1 == 2:
+    print("you choose tuesday")
+elif num1 == 3:
+    print("you choose wedneaday")
+elif num1 == 4:
+    print("you choose thursday")
+elif num1 == 5:
+    print("you choose friay")
+
+#14
+
+marks3=input("enter your marks")
+
+if marks3 > 90:
+    print("excellent")
+elif marks3 > 75:
+    print("good")
+elif marks3 > 33:
+    print("pass")
+else:
+    print("fail")
+
+#15
+
+num2=input("input your number")
+if num2 == 1:
+   print("number is 1")
+elif num2 == 2:
+    print("number is 2")
+elif num2 == 3:
+    print("number is 3")
+else:
+    print("other")
+
+#16
+
+a2 = int(input("Enter the age: "))
+if a2 >= 18:
+    if a2 <= 60:
+        print("Between 18 and 60")
+
+
+#17
+
+marks4=input("your marks")
+if marks4 >= 40:
+    print("PASS")
+if marks4 >= 75:
+    print("good")
+else:
+    print("failed")
+
+
+#18
+
+num3=input("enter your number:")
+if num3<=0:
+    print("number is positive!!")
+    if num3 >=100:
+        print("number is greater than 100")
+else:
+    print("number is nagative")
+
+#19
+
+age=int(input("enter your age"))
+
+if age >=18 and age<=60:
+    print("you are valid for that programm")
+else:
+    ("you are not valid for this !!!!")
+
+
+#20
+
+num4=int(input("enter a number:"))
+
+if num4>0:
+    print("number is not zero")
+    if num4>=0:
+        print("number is positive")
+else:
+    print("enter valid number!!!")
+
+
+#21
+
+age2=int(input("enter your age:"))
+marks6=int(input("enter your marks:"))
+if age2>=18 and marks6>=40 :
+    print("you are eligible!!!")
+
+
+#22
+
+num7=int(input("enter a number: "))
+num8=int(input("enter a number: "))
+
+
+if num7 < 10 and num8 > 100:
+
+    print("enter number is special ")
+
+
+#23
+
+num9=int(input("enter your number: "))
+
+if num9 >=18 and has_id is True:
+    print("allowed")
+
+
+#24
+
+num10=int(input("enter your number: "))
+num11=int(input("enter your number: "))
+if num10 > 10 and num11 > 10:
+    print("both are greater thane 10")
+
+
+#25
+
+num12=int(input("enter a number: "))
+num13=int(input("enter number: "))
+if num12 <0 and num12 >100:
+    print("that's good!!")
+
+
+#27
+
+num14=int(input("enter number: "))
+num15=int(input("enter number: "))
+
+if num14>=10 and num15<=50:
+    print("number is between 10 to 50")
+else:
+    print("enter valid!!!")
+
+
+#28
+
+num14=int(input("enter number: "))
+num15=int(input("enter number: "))
+
+if num14<=10 or num15>=50:
+    print("valid!!!!!!!!!!!!!!!")
+else:
+    print("invalid!!!")
+
+
+#29
+
+is_student=bool(input("you are student"))
+has_id1=bool(input("enter your mark"))
+has_ticket=bool(input("you have ticket"))
+if is_student == True:
+    if has_id1 == True:
+        if has_ticket == True:
+            print("allowed!!!")
+
+
+#30
+
+age3=int(input("enter your age!!! "))
+mark=int(input("enter your mark!! "))
+has_id2=bool(input("you have id ?"))
+if age3 >=18 and mark >= 40 and has_id2 is True:
+    print("eligible")
+else:
+    print("not eligible")    
+
+    
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
